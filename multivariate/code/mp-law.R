e8336159fe870541cb6850d1a317faf0a16824c1
Patrick Perry <patperry@stanford.edu>
2009-05-04 21:01:44 -0700

    add code for generating mp-law plot

---
+++ multivariate/code/mp-law.R
@@ -0,0 +1,25 @@
+require( "RMTstat" )
+require( "plyr" )
+require( "ggplot2" )
+
+gamma <- c( 0.25, 1.00, 4.00 )
+
+mp <- ldply( gamma, 
+          function( g, ... ) { 
+              a <- ( 1 - sqrt( 1/g ) )^2; 
+              b <- ( 1 + sqrt( 1/g ) )^2; 
+              x <- seq(a, b, len=100 ); 
+              y <- dmp( x, conc=g ); 
+              data.frame( concentration=g, quantile=x, density=y ) } )
+         
+p <- ( ggplot( mp, aes( quantile, density, color=concentration ) )
+       + labs( x="Quantile", y="Density", colour="Concentration" ) 
+       + geom_line( aes( group=concentration ) )
+       + scale_colour_gradient( breaks=sort( gamma, decreasing=TRUE ) )
+       + theme_bw()
+     )
+p
+
+pdf( file="../plots/mp-law.pdf", width=6, height=4 )
+print( p )
+dev.off()
